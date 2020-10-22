d127c0b65375fc14bee12971dd5795ae696f632b
littleroys <qhluo@thoughtworks.com>
2020-10-22 16:59:50 +0800

    [roys] update 557 reverse string

--- src/string/reverse_word_iii.rs
+++ src/string/reverse_word_iii.rs
@@ -19,12 +19,9 @@ impl Solution {
                 unsafe {
                     std::ptr::swap(&mut v[right - 1], &mut v[left]);
                 }
-                left += 1;
-                right -= 1;
+                left += 1; right -= 1;
             }
-
-            left = next;
-            fright = next;
+            left = next;right = next;
         }
 
         String::from_utf8(v).unwrap()
