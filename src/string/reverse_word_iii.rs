1355fbb571e67b482351e1145fa51eb4f019e361
littleroys <qhluo@thoughtworks.com>
2020-10-22 16:58:16 +0800

    [roys] update 557 reverse string

--- src/string/reverse_word_iii.rs
+++ src/string/reverse_word_iii.rs
@@ -5,7 +5,29 @@ struct Solution;
 
 impl Solution {
     pub fn reverse_words(s: String) -> String {
-        s
+        let mut v = s.into_bytes();
+        let (mut right, mut left) = (0, 0);
+
+        while right < v.len() {
+            while right < v.len() && v[right] != ' ' as u8 {
+                right += 1;
+            }
+
+            let next = right + 1;
+
+            while left < right {
+                unsafe {
+                    std::ptr::swap(&mut v[right - 1], &mut v[left]);
+                }
+                left += 1;
+                right -= 1;
+            }
+
+            left = next;
+            fright = next;
+        }
+
+        String::from_utf8(v).unwrap()
 
         // s.split_whitespace()
         //     .map(|word| word.chars().rev().collect::<String>() + " ")
@@ -23,7 +45,7 @@ mod test {
     pub fn test() {
         assert_eq!(
             Solution::reverse_words("Let's take LeetCode contest".to_string()),
-            "s'teL   ekat edoCteeL tsetnoc".to_string()
+            "s'teL ekat edoCteeL tsetnoc".to_string()
         );
     }
 }
