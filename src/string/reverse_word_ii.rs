f5c36aa1ac90387f1f8c50a2a6aa11eb5f6ed185
littleroys <qhluo@thoughtworks.com>
2020-10-23 15:20:51 +0800

    [roys] add #541 reverse stringII

--- src/string/reverse_word_ii.rs
+++ src/string/reverse_word_ii.rs
@@ -4,7 +4,28 @@ struct Solution;
 
 impl Solution {
     pub fn reverse_str(s: String, k: i32) -> String {
-        "".to_string()
+        let mut v = s.into_bytes();
+        let (mut i, n) = (0, v.len());
+        let mut reverse = false;
+        while i < n {
+            let mut start = i;
+            while i < n && i < (start + k as usize) {
+                i += 1;
+            }
+
+            let (mut left, mut right) = (start, i - 1);
+            reverse = !reverse;
+
+            while left < right && reverse {
+                unsafe {
+                    std::ptr::swap(&mut v[left], &mut v[right]);
+                }
+                left += 1;
+                right -= 1;
+            }
+        }
+
+        String::from_utf8(v).unwrap()
     }
 }
 
